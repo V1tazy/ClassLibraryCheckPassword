8f2d06b05c123126af8dbd5027eab91068ab3a25
V1tazy <slavasenotrusov2005@gmail.com>
2024-05-07 23:03:51 +0900

    Добавьте файлы проекта.

---
+++ PasswordCheker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCheckPassword
+{
+    public class PasswordCheker
+    {
+        public static bool ValidatePassword(string password) 
+        {
+            if(password.Length < 8 || password.Length > 25)
+                return false;
+
+            if(!password.Any(Char.IsLower))
+                return false;
+            if(!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (password.Intersect("#$%^&_").Count() == 0)
+                return false;
+
+
+            return true;
+        }
+    }
+}
